41a27410d9f6e7694a89f26bff76c450facadc7f
Archy Brendah <ondiekbrendah@gmail.com>
2019-06-26 16:34:46 +0300

    settings

--- DClassroom1/DClassroom1/settings.py
+++ DClassroom1/DClassroom1/settings.py
@@ -156,10 +156,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 AUTH_USER_MODEL = 'myFile.User'
+
 STATIC_URL = '/static/'
-# STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=[
 os.path.join(BASE_DIR,'static')]
+
+
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 LOGIN_URL = 'login'
